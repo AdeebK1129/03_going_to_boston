12e67b504f35a3a338822cc874b3b9e38d90510b
Adeeb <adeeb19909@gmail.com>
2023-11-10 14:51:21 -0500

    updated input features

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
-function isValid() {
-    let userRounds = document.getElementById("rounds").value;
-    if (userRounds % 2 == 0) {
-        alert("Number of rounds must be odd");
-    }
-    else {
-
-    }
-
-}
-
-
+/** 
+ * 
 document.getElementById("myForm").addEventListener("submit", function (event) {
     event.preventDefault();
     let userRounds = document.getElementById("rounds").value;
@@ -20,3 +10,33 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
 
     }
 });
+
+*/
+
+const playerInfo = document.querySelector("#prompt");
+let player1Score = document.getElementById("p1score");
+let player1Wins = document.getElementById("p1Wins");
+let player2Score = document.getElementById("p2score");
+let player2Wins = document.getElementById("p2Wins");
+let userRounds = document.getElementById("rounds").value;
+const gameInfo_class_list = document.querySelector(".gameInfo").classList; 
+
+
+function startGame() {
+    let counter = 0;
+    gameInfo_class_list.remove("hidden"); 
+}
+
+
+function isValid() {
+    let userRounds = document.getElementById("rounds").value;
+    if (userRounds % 2 == 0) {
+        alert("Number of rounds must be odd");
+    }
+    else {
+        startGame();
+    }
+
+}
+
+document.getElementById("submit").addEventListener("click", isValid);
